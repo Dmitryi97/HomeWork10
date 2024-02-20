f15ef7e2ecda60cfa6080e0594322c039cf3d124
Dmitryi <pytin97@bk.ru>
2024-02-20 07:25:40 +0300

    сделал Дз!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Задача1
+        System.out.println("Задача1");
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastame = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastame;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        //Задача2
+        System.out.println("Задача2");
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        //Задача3
+        System.out.println("Задача3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё","е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName);
     }
 }
